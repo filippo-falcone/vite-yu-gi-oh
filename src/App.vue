e3486be1b8fdb776576577c407ae3b7480efbf2b
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-15 14:54:00 +0200

    feat: add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios';
 import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
+  components: {
+    AppHeader
+  },
   data() {
     return {
       store
@@ -11,7 +15,9 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+  <AppHeader></AppHeader>
+</template>
 
 <style lang="scss">
 @use './style/generic.scss';
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar">
+            <div class="container-fluid justify-content-start fs-5">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/yu-gi-oh-logo.svg" alt="Yu-Gi-Oh!" width="200px">
+                </a>
+                Yu-Gi-Oh! Api
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+header {
+    background-color: $brand-light;
+}
+</style>
