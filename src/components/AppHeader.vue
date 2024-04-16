cd25c2100dcfc39881d8c03fadd576a8319646c7
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-16 10:09:03 +0200

    fix: add d-none to div on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,11 +7,11 @@ export default {
 <template>
     <header>
         <nav class="navbar">
-            <div class="container-fluid justify-content-start fs-5">
+            <div class="container-fluid justify-content-center justify-content-md-start fs-5">
                 <a class="navbar-brand" href="#">
                     <img src="../assets/img/yu-gi-oh-logo.svg" alt="Yu-Gi-Oh!" width="200px">
                 </a>
-                Yu-Gi-Oh! Api
+                <div class="d-none d-md-block">Yu-Gi-Oh! Api</div>
             </div>
         </nav>
     </header>
